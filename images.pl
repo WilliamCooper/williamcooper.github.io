8026801df2b25f4dc052f08529d0c10cc76601f8
WilliamCooper <cooperw@ucar.edu>
2022-03-22 13:51:35 -0600

    Mods for bookdown version

--- images.pl
+++ images.pl
@@ -2,17 +2,9 @@
 # Associate images original text with physical files.
 
 
-$key = q/(2pislashV)(CP(nu)nu^{5slash3})^{3slash2};MSF=1.6;LFS=12;AAT/;
+$key = q/bgroupcolor{fgcolor}(2pislashV)(CP(nu)nu^{5slash3})^{3slash2}egroup;MSF=1.6;LFS=12;AAT/;
 $cached_env_img{$key} = q|<IMG STYLE="height: 2.62ex; vertical-align: -0.65ex; " SRC="|."$dir".q|img17.png"
- ALT="$(2\pi/V)(CP(\nu)\nu^{5/3})^{3/2}$">|; 
-
-$key = q/C=1.5;MSF=1.6;LFS=12;AAT/;
-$cached_env_img{$key} = q|<IMG STYLE="height: 1.62ex; vertical-align: -0.13ex; " SRC="|."$dir".q|img19.png"
- ALT="$C=1.5$">|; 
-
-$key = q/V;MSF=1.6;LFS=12;AAT/;
-$cached_env_img{$key} = q|<IMG STYLE="height: 1.51ex; vertical-align: -0.13ex; " SRC="|."$dir".q|img18.png"
- ALT="$V$">|; 
+ ALT="\bgroup\color{fgcolor}$(2\pi/V)(CP(\nu)\nu^{5/3})^{3/2}$\egroup">|; 
 
 $key = q/bgroupcolor{fgcolor}-5slash3egroup;MSF=1.6;LFS=12;AAT/;
 $cached_env_img{$key} = q|<IMG STYLE="height: 2.23ex; vertical-align: -0.65ex; " SRC="|."$dir".q|img14.png"
@@ -22,6 +14,10 @@ $key = q/bgroupcolor{fgcolor}C(nu)egroup;MSF=1.6;LFS=12;AAT/;
 $cached_env_img{$key} = q|<IMG STYLE="height: 2.23ex; vertical-align: -0.65ex; " SRC="|."$dir".q|img2.png"
  ALT="\bgroup\color{fgcolor}$C(\nu)$\egroup">|; 
 
+$key = q/bgroupcolor{fgcolor}C=1.5egroup;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.62ex; vertical-align: -0.13ex; " SRC="|."$dir".q|img19.png"
+ ALT="\bgroup\color{fgcolor}$C=1.5$\egroup">|; 
+
 $key = q/bgroupcolor{fgcolor}P(nu)egroup;MSF=1.6;LFS=12;AAT/;
 $cached_env_img{$key} = q|<IMG STYLE="height: 2.23ex; vertical-align: -0.65ex; " SRC="|."$dir".q|img12.png"
  ALT="\bgroup\color{fgcolor}$P(\nu)$\egroup">|; 
@@ -29,6 +25,10 @@ $cached_env_img{$key} = q|<IMG STYLE="height: 2.23ex; vertical-align: -0.65ex; "
 $key = q/bgroupcolor{fgcolor}T(x)egroup;MSF=1.6;LFS=12;AAT/;
 $cached_env_img{$key} = q|<IMG STYLE="height: 2.23ex; vertical-align: -0.65ex; " SRC="|."$dir".q|img7.png"
  ALT="\bgroup\color{fgcolor}$T(x)$\egroup">|; 
+
+$key = q/bgroupcolor{fgcolor}Vegroup;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.51ex; vertical-align: -0.13ex; " SRC="|."$dir".q|img18.png"
+ ALT="\bgroup\color{fgcolor}$V$\egroup">|; 
 
 $key = q/bgroupcolor{fgcolor}ln(10)log_{10}nuapprox2.30log_{10}nuegroup;MSF=1.6;LFS=12;AAT/;
 $cached_env_img{$key} = q|<IMG STYLE="height: 2.29ex; vertical-align: -0.68ex; " SRC="|."$dir".q|img11.png"
@@ -54,6 +54,10 @@ $key = q/bgroupcolor{fgcolor}nuP(nu)egroup;MSF=1.6;LFS=12;AAT/;
 $cached_env_img{$key} = q|<IMG STYLE="height: 2.23ex; vertical-align: -0.65ex; " SRC="|."$dir".q|img9.png"
  ALT="\bgroup\color{fgcolor}$\nu P(\nu)$\egroup">|; 
 
+$key = q/bgroupcolor{fgcolor}nu^{5slash3}egroup;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 2.17ex; vertical-align: -0.13ex; " SRC="|."$dir".q|img16.png"
+ ALT="\bgroup\color{fgcolor}$\nu^{5/3}$\egroup">|; 
+
 $key = q/bgroupcolor{fgcolor}nuegroup;MSF=1.6;LFS=12;AAT/;
 $cached_env_img{$key} = q|<IMG STYLE="height: 1.23ex; vertical-align: -0.13ex; " SRC="|."$dir".q|img3.png"
  ALT="\bgroup\color{fgcolor}$\nu$\egroup">|; 
@@ -65,10 +69,6 @@ $cached_env_img{$key} = q|<IMG STYLE="height: 2.23ex; vertical-align: -0.65ex; "
 $key = q/bgroupcolor{fgcolor}x=lnnuegroup;MSF=1.6;LFS=12;AAT/;
 $cached_env_img{$key} = q|<IMG STYLE="height: 1.56ex; vertical-align: -0.13ex; " SRC="|."$dir".q|img6.png"
  ALT="\bgroup\color{fgcolor}$x=\ln\nu$\egroup">|; 
-
-$key = q/nu^{5slash3};MSF=1.6;LFS=12;AAT/;
-$cached_env_img{$key} = q|<IMG STYLE="height: 2.17ex; vertical-align: -0.13ex; " SRC="|."$dir".q|img16.png"
- ALT="$\nu^{5/3}$">|; 
 
 $key = q/{displaymath}bgroupcolor{fgcolor}P(nu)=frac{dC(nu)}{dnu},,,,.egroup{displaymath};MSF=1.6;LFS=12;AAT/;
 $cached_env_img{$key} = q|<IMG STYLE=""
